f9a8609c71e3241d83949f1b970f058d7716afb6
Tajbir islam <studenttajbirislam@gmail.com>
2025-07-10 14:02:39 +0600

    test

--- handler/runWork.js
+++ handler/runWork.js
@@ -15,7 +15,7 @@ const runWork = async (url) => {
     let proxyFile = fs.readFileSync('uploads/proxy.txt', 'utf-8')
     let proxies = proxyFile.split('\n').map(line => line.trim()).filter(line => line !== '')
 
-    const BATCH_SIZE = 4;
+    const BATCH_SIZE = 6;
     let index = 0;
     let active = [];
 
